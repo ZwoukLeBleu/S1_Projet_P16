467af406aff6e103c852004ce44c69cd6a86e6d4
Arballio <arthur.blanchard022@gmail.com>
2024-09-25 18:15:09 -0400

    test

--- Projet Parcours P16 S1/src/main.cpp
+++ Projet Parcours P16 S1/src/main.cpp
@@ -1,36 +1,23 @@
 /*
-Projet: Le nom du script
-Equipe: Votre numero d'equipe
-Auteurs: Les membres auteurs du script
-Description: Breve description du script
-Date: Derniere date de modification
+Projet: Imprimante
+Equipe: P16
+Auteurs: Arthur, BLAA8729
+Description: Fichier principale
+Date: 25 sept
 */
 
-/*
-Inclure les librairies de functions que vous voulez utiliser
-*/
+
 #include <LibRobus.h>
 #include <Arduino.h>
 
-/*
-Variables globales et defines
- -> defines...
- -> L'ensemble des fonctions y ont acces
-*/
 
 
-bool bumperArr;
-int vertpin = 53;
-int rougepin = 49;
 bool vert = false;
 bool rouge = false;
 int etat = 0; // = 0 arrêt 1 = avance 2 = recule 3 = TourneDroit 4 = TourneGauche
 int etatPast = 0;
 float vitesse = 0.40;
 
-/*
-Vos propres fonctions sont creees ici
-*/
 
 void beep(int count){
   for(int i=0;i<count;i++){
@@ -67,84 +54,16 @@ void tourneGauche(){
   MOTOR_SetSpeed(LEFT, 0.5*vitesse);
 };
 
-/*
-Fonctions d'initialisation (setup)
- -> Se fait appeler au debut du programme
- -> Se fait appeler seulement un fois
- -> Generalement on y initilise les varibbles globales
-*/
 void setup(){
   BoardInit();
   
-  //initialisation
-  pinMode(vertpin, INPUT);
-  pinMode(rougepin, INPUT);
+
   delay(100);
   beep(3);
 }
 
-/*
-Fonctions de boucle infini
- -> Se fait appeler perpetuellement suite au "setup"
-*/
-void loop() {
-  etatPast = etat;
-  bumperArr = ROBUS_IsBumper(3);
-  if (bumperArr){
-    if (etat == 0){
-      beep(2);
-      etat = 1;
-    } 
-    else{
-      beep(1);
-      etat = 0;
-    }
-  }
-  
-  vert = digitalRead(vertpin);
-  rouge = digitalRead(rougepin);
-  if (etat > 0){
-    if (vert && rouge){ // aucun obstacle => avance
-      etat = 1;
-    }
-    if (!vert && !rouge){  // obstacle devant => recule
-      etat = 2;
-    }
-    if (!vert && rouge){ // obstacle à gauche => tourne droit
-        etat = 3;
-      }
-    if (vert && !rouge){ // obstacle à droite => tourne gauche
-        etat = 4;
-    }
-  }
 
-  if (etatPast != etat){
-    arret();
-    delay(50);
-  }
-  else{
-    switch (etat)
-    {
-    case 0:
-      arret();
-      break;
-    case 1:
-      avance();
-      break;
-    case 2:
-      recule();
-      break;
-    case 3:
-      tourneDroit();
-      break;
-    case 4:
-      tourneGauche();
-      break;            
-    default:
-      avance();
-      etat = 1;
-    break;
-    }
-  }
-  delay(200);
+void loop() {
+
+
 }
